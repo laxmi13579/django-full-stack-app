8548bd5faa927506134513b57e9c1a1855e929d6
laxmi13579 <laxmi.yadav370515@gmail.com>
2024-12-29 16:46:18 +0530

    template

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ogncx**g+=c25n%v9x01wa+&6f%11j)&i*7%*@ksw0%-lxf6z*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
