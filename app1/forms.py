1c3e715a7646c222313649fa8cde00f5528a3bb8
laxmi13579 <laxmi.yadav370515@gmail.com>
2025-01-12 22:32:30 +0530

    builtin validator for name lenght  and custom validator for password lenght

--- app1/forms.py
+++ app1/forms.py
@@ -1,25 +1,31 @@
 from django import forms 
-from django.core.validators import validate_email
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 import re
 
+# custom validator to check the length of password
+# built in validator to check the length of name
+def password_validator(value):
+    if len(value) < 4:
+        raise forms.ValidationError('Password should be greater than 4 digit')
 class RegisterForm(forms.Form):
-    name = forms.CharField(max_length=100)
+    name = forms.CharField(validators=[MinLengthValidator(5), MaxLengthValidator(100)])
     email = forms.CharField(max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput, validators=[password_validator])
+
 
 # validate with all field, below code use name field validation
-    def clean(self):
-        cleaned_data = super().clean()
-        name_value = cleaned_data.get('name')
-        password_value = cleaned_data.get('password')
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     name_value = cleaned_data.get('name')
+    #     password_value = cleaned_data.get('password')
 
-        if name_value and len(name_value) < 4:
-            self.add_error('name','Enter name greater than 4 character')
+    #     if name_value and len(name_value) < 4:
+    #         self.add_error('name','Enter name greater than 4 character')
 
-        if password_value and len(password_value) < 4:
-            self.add_error('password','Enter password greater than 4 character')
+    #     if password_value and len(password_value) < 4:
+    #         self.add_error('password','Enter password greater than 4 character')
 
-        return cleaned_data
+    #     return cleaned_data
 
 # validate with specific field, below code use name field validation
 # class RegisterForm(forms.Form):
