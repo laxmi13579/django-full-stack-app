a8d5f205e2da627edd6677aca03c853d9912606b
laxmi13579 <laxmi.yadav370515@gmail.com>
2025-01-15 22:40:24 +0530

    StudentRegisterForm using ModelForm

--- app1/admin.py
+++ app1/admin.py
@@ -4,4 +4,9 @@ from app1.models import Profile
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'password']
+    list_display = ['student_name', 'teacher_name', 'email', 'password']
+
+
+# @admin.register(Student)
+# class StudentAdmin(admin.ModelAdmin):
+#     list_display = ['student_id', 'first_name', 'admission_date']
